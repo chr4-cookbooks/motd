e704ab069f9e6b886afd3f94ed17d3007625a0dd
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2020-04-29 00:32:23 +0100

    Run cookstyle -a

--- metadata.rb
+++ metadata.rb
@@ -10,6 +10,6 @@ source_url       'https://github.com/chr4-cookbooks/motd'
 issues_url       'https://github.com/chr4-cookbooks/motd/issues'
 license          'GPL-3.0'
 
-%w(ubuntu debian).each do |os|
-  supports os
-end
+supports 'centos', '>= 7.0'
+supports 'debian', '>= 10.0'
+supports 'ubuntu', '>= 20.04'
